f1a6e74773fdbd143879ae5e260d1396284a5f71
stefian <v_ianta@yahoo.com>
2019-05-17 12:12:27 -0400

    marked task 8 done

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
                     <li>&#10004; Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
                     <li>&#10004; Load the "Course" component as a nested component of "Courses"</li>
                     <li>&#10004; Add a 404 error page and render it for any unknown routes</li>
-                    <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
+                    <li>&#10004; Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
                 </ol>
                 <nav>
                     <ul style={{ listStyle: 'none', margin: 'auto', padding: '0' }}>

f1ba7e485518ab35aa3f4773abbbd363855dbf27
stefian <v_ianta@yahoo.com>
2019-05-17 10:51:13 -0400

    set state courseTitle to param[1] in compDidMount iterator and render state.courseTitle in h1

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -9,14 +9,16 @@ class Course extends Component {
         console.log(this.props);
         const query = new URLSearchParams(this.props.location.search);
         for (let param of query.entries()) {
-            console.log(param);
+            // console.log(param);
+            this.setState({courseTitle: param[1]});
         };
     }
 
     render () {
         return (
             <div>
-                <h1>{this.props.match.params.courseTitle}</h1>
+                {/* <h1>{this.props.match.params.courseTitle}</h1> */}
+                <h1>{this.state.courseTitle}</h1>
                 <p>You selected the Course with ID: {this.props.match.params.courseId}</p>
             </div>
         );
